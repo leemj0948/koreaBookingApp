d48a1ba4a4ff74cedc7431297f93060c07e4676f
MJ <mj@MJui-MacBookPro.local>
2022-09-16 17:53:13 +0900

    add:글로벌 css 삽입, 결제 성공 페이지 디자인

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import Course from './body/Course';
+import PaySuccess from './body/PaySuccess';
 //쿼리 파라미터로 바뀐 제목 별로 다른 페이지 보이기
 const Main = () => {
   return (
-    <>
-      {/* <MainBody>
+    <MainBody>
+      {/* 
  
         <BackgroundImg>
           // <Title>I am here to help you guys.</Title>
@@ -13,16 +14,18 @@ const Main = () => {
       
         </BackgroundImg>
    
-      </MainBody> */}
-      <Course />
-    </>
+       */}
+      {/* <Course /> */}
+      <PaySuccess />
+    </MainBody>
   );
 };
 export default Main;
 
 const MainBody = styled.section`
-  width: 100%;
-  height: 100%;
+  margin: 0;
+  max-width: 375px;
+  height: 542px;
 `;
 const BackgroundImg = styled.section``;
 const Title = styled.p``;
--- src/pages/body/PaySuccess.js
+++ src/pages/body/PaySuccess.js
@@ -1,5 +1,46 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const PaySuccess = () => {};
+const PaySuccess = () => {
+  return (
+    <BackGround>
+      <Header>Thank you</Header>
+      <Body>
+        <p className="start">Let's start</p>
+        <p className="korean">Korean journey</p>
+        <p className="go">Go to booking page</p>
+      </Body>
+    </BackGround>
+  );
+};
+const BackGround = styled.section`
+  display: block;
+  width: 100%;
+  height: 100%;
+  background-color: #eef5f1;
+`;
+const Header = styled.h1`
+  margin: 0;
+  padding: 6rem 1rem 0;
+  font-size: 40px;
+  text-align: center;
+`;
+const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 2rem 5rem;
+  width: 227px;
+  height: 154px;
+  font-size: 24px;
+  font-family: 'Brandon Grotesque, Regular', Regular;
+  .start {
+    text-align: left;
+  }
+  .korean {
+    text-align: right;
+  }
+  .go {
+    text-align: left;
+  }
+`;
 export default PaySuccess;
