babdb69abd1707c26066cd09efdb12867f17fa0c
MJ <mj@MJui-MacBookPro.local>
2022-09-21 18:32:50 +0900

    add: mypage 디자인 작업중

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import Course from './body/Course';
 //쿼리 파라미터로 바뀐 제목 별로 다른 페이지 보이기
 const Main = () => {
   return (
     <MainBody>
-      {/* 
- 
-        <BackgroundImg>
-          // <Title>I am here to help you guys.</Title>
-          // <NextButton />
-      
-        </BackgroundImg>
-   
-       */}
-      <Course />
+      <BackgroundImg>
+        <Title>I am here to help you guys.</Title>
+        <NextButton />
+      </BackgroundImg>
     </MainBody>
   );
 };
@@ -24,6 +17,7 @@ const MainBody = styled.section`
   margin: 0;
   max-width: 375px;
   height: 542px;
+  overflow: scroll;
 `;
 const BackgroundImg = styled.section``;
 const Title = styled.p``;
---
+++ src/pages/utilty/ScheduleCardList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ScheduleCardList = () => {
+  // const DummyClass = [
+  //   { nth: '4th', day: 'Thr', time: '10:00-10:50' },
+  //   { nth: '8th', day: 'Mon', time: '10:00-10:50' },
+  //   { nth: '10th', day: 'Wed', time: '13:00-13:50' },
+  // ];
+
+  return (
+    <Schedule>
+      {/* <CardList>dfds</CardList> */}
+      {/* <ScheduleCard>
+        <Month>
+          <p className="nth">nth</p>
+          <p className="day">day</p>
+        </Month>
+        <ClassTime>time</ClassTime>
+      </ScheduleCard> */}
+    </Schedule>
+  );
+};
+const Schedule = styled.div``;
+const CardList = styled.div;
+// const Month = styled.div``;
+// const ClassTime = styled.div``;
+export default ScheduleCardList;
